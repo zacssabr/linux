465c2ce249bce0c4385f7994af2ba864b7f0a7cd
zacssabr <zacssa@gmail.com>
2020-10-04 08:17:29 -0300

    Create comandos.sh

---
+++ SNMP/comandos.sh
@@ -0,0 +1,17 @@
+##### WALK e TRANSLATE ####
+
+snmpwalk -v2c -c <COMUNITY> <IP> -On <OID>
+snmptranslate -Tz -m ./<MIB>
+
+
+##### DIRETORIO SNMP ######
+
+mkdir -p $HOME/.snmp
+for i in `ls -d /usr/share/snmp/mibs/vendors/*/`; do
+ echo "mibdirs +$i" > $HOME/.snmp/snmp.conf
+ export MIBDIRS=$MIBDIRS:$i
+done
+
+##### EXPORT MIBS ######
+
+export MIBS=+ALL
